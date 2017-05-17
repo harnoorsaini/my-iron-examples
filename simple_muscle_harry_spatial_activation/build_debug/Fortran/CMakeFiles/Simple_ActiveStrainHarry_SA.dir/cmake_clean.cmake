50f7979cc5e53d6ce54a2dcbdaa92f21e3f5dcad
Harnoor Saini <saini@mechbau.uni-stuttgart.de>
2017-05-17 11:20:49 +0200

    clean up and view.com update

---
+++ simple_muscle_harry_spatial_activation/build_debug/Fortran/CMakeFiles/Simple_ActiveStrainHarry_SA.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Simple_ActiveStrainHarry_SA.dir/src/Simple_ActiveStrainHarry_SA.f90.o"
+  "Simple_ActiveStrainHarry_SA.pdb"
+  "Simple_ActiveStrainHarry_SA"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/Simple_ActiveStrainHarry_SA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ simple_muscle_harry_spatial_activation/build_debug/Fortran/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /usr/local/opencmiss/examples/simple_muscle_harry_spatial_activation/Fortran
+# Build directory: /usr/local/opencmiss/examples/simple_muscle_harry_spatial_activation/build_debug/Fortran
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Simple_ActiveStrainHarry_SA "/usr/local/opencmiss/examples/simple_muscle_harry_spatial_activation/build_debug/Fortran/Simple_ActiveStrainHarry_SA")
+set_tests_properties(Simple_ActiveStrainHarry_SA PROPERTIES  ENVIRONMENT "LD_LIBRARY_PATH=/usr/local/opencmiss/iron/install/x86_64_linux/gnu-5.4-F5.4/openmpi_release/release/bin:")
